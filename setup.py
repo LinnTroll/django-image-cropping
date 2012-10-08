9b95f4ca5e6f6eb6cd263ebfdcfa714d235e3fa8
LinnTroll <pavel.hoodoleev@gmail.com>
2012-10-08 23:43:29 +0300

    fix setup

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(name = "django-image-cropping",
     packages = find_packages(),
     include_package_data=True,
     install_requires = [
-        'easy_thumbnails',
+        'sorl-thumbnail',
     ],
     classifiers = [
         'Development Status :: 4 - Beta',
